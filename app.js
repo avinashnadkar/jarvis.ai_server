e83ca7ffc3f03f7a45559ddfd2873489600277a8
avinashnadkar <avinashnadkar3@gmail.com>
2023-08-12 17:24:01 +0530

    Initialized the project using npm init and installed express.js framework to create a server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express ();
+app.use(express.json());
+
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/status', (request, response) => {
+    const status = {
+       'Status': 'Running on port no. 2000'
+    };
+    
+    response.send(status);
+ });
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+
